6d55c5b7ce488c2fcf250943da6f0f53bcda7e15
FgalUR <galetskifedor@gmail.com>
2024-12-08 15:23:45 +0300

    Update TestProject.cpp

--- TestProject/TestProject.cpp
+++ TestProject/TestProject.cpp
@@ -13,7 +13,14 @@ int main()
     cout << "HSE FCS AMI d";
     int first;
     int second;
-    int third;
+    <<<<<< < HEAD
+        double trouble;
+    ====== =
+        int third;
+    <<<<<<< Fedor
+        ====== =
+
+        >>>>>> > 02c8d90188680a1b80ae77bd48cac3f10f71b327
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
